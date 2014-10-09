a8808307526dc13bcbe63322af01b9e28b607a47
whlo <whloxx@gmail.com>
2014-10-09 14:47:17 +0900

    firstbuild

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace DualCamCap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.captureCheckBox = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.savePictureBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(347, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // captureCheckBox
+            // 
+            this.captureCheckBox.AutoSize = true;
+            this.captureCheckBox.Location = new System.Drawing.Point(687, 12);
+            this.captureCheckBox.Name = "captureCheckBox";
+            this.captureCheckBox.Size = new System.Drawing.Size(64, 16);
+            this.captureCheckBox.TabIndex = 2;
+            this.captureCheckBox.Text = "Capture";
+            this.captureCheckBox.UseVisualStyleBackColor = true;
+            this.captureCheckBox.CheckedChanged += new System.EventHandler(this.captureCheckBox_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(687, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(19, 19);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(687, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(19, 19);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "1";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(712, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Index1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(712, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Index2";
+            // 
+            // savePictureBtn
+            // 
+            this.savePictureBtn.Location = new System.Drawing.Point(687, 111);
+            this.savePictureBtn.Name = "savePictureBtn";
+            this.savePictureBtn.Size = new System.Drawing.Size(75, 23);
+            this.savePictureBtn.TabIndex = 7;
+            this.savePictureBtn.Text = "画像の保存";
+            this.savePictureBtn.UseVisualStyleBackColor = true;
+            this.savePictureBtn.Click += new System.EventHandler(this.savePictureBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "カメラ1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(345, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "カメラ2";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 279);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "カメラ3";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(687, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(19, 19);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.Text = "2";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(712, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Index3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(687, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 531);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.savePictureBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.captureCheckBox);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "マルチカメラ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox captureCheckBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button savePictureBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System.Collections;
+
+namespace DualCamCap
+{
+    public partial class Form1 : Form
+    {
+
+        IplImage frame1, frame2, frame3;
+        bool finishFlag = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void captureCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            Bitmap cam1 = null;
+            Bitmap cam2 = null;
+            Bitmap cam3 = null;
+            captureBmp(ref cam1, ref cam2, ref cam3);
+        }
+
+        private void savePictureBtn_Click(object sender, EventArgs e)
+        {
+            Application.DoEvents();
+            if(captureCheckBox.Checked == true)
+            {
+                frame1.SaveImage("test.png");
+                frame2.SaveImage("test2.png");
+                frame3.SaveImage("test3.png");
+            }
+            else
+            {
+                MessageBox.Show("カメラがキャプチャモードになっていません。",
+                    "エラー",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        //Webカメラ画像取得用メソッド
+        private void captureBmp(ref Bitmap bitmap1, ref Bitmap bitmap2, ref Bitmap bitmap3)
+        {
+            //カメラのインデックス番号の取得
+            //通常0から始まる連番のはず
+            try
+            {
+                using (CvCapture capture1 = Cv.CreateCameraCapture(int.Parse(textBox1.Text)))
+                using (CvCapture capture2 = Cv.CreateCameraCapture(int.Parse(textBox2.Text)))
+                using (CvCapture capture3 = Cv.CreateCameraCapture(int.Parse(textBox3.Text)))
+                {
+                    //カメラ初期化で時間が掛かると例外エラーが出るので1秒停止する
+                    System.Threading.Thread.Sleep(1000);
+
+                    //リサイズ画像格納用変数
+                    IplImage frameResized1 = Cv.CreateImage(Cv.Size(pictureBox1.Width, pictureBox1.Height), BitDepth.U8, 3);
+                    IplImage frameResized2 = Cv.CreateImage(Cv.Size(pictureBox2.Width, pictureBox2.Height), BitDepth.U8, 3);
+                    IplImage frameResized3 = Cv.CreateImage(Cv.Size(pictureBox3.Width, pictureBox3.Height), BitDepth.U8, 3);
+                    //チェックボックスがONの時キャプチャし続ける
+                    while (captureCheckBox.Checked == true && finishFlag == false)
+                    {
+                        //カメラ1のキャプチャからリサイズ、表示
+                        frame1 = Cv.QueryFrame(capture1);
+                        Cv.Resize(frame1, frameResized1, Interpolation.Lanczos4);
+                        bitmap1 = BitmapConverter.ToBitmap(frameResized1);
+                        pictureBox1.Image = bitmap1;
+                        //カメラ2のキャプチャからリサイズ、表示
+                        frame2 = Cv.QueryFrame(capture2);
+                        Cv.Resize(frame2, frameResized2, Interpolation.Lanczos4);
+                        bitmap2 = BitmapConverter.ToBitmap(frameResized2);
+                        pictureBox2.Image = bitmap2;
+                        //カメラ3のキャプチャからリサイズ、表示
+                        if (capture3 == null)
+                        {
+                            label5.Text = "down";
+                        }
+                        else
+                        {
+                            frame3 = Cv.QueryFrame(capture3);
+                            Cv.Resize(frame3, frameResized3, Interpolation.Lanczos4);
+                            bitmap3 = BitmapConverter.ToBitmap(frameResized3);
+                            pictureBox3.Image = bitmap3;
+                        }
+                        //画面更新
+                        Application.DoEvents();
+                    }
+                    Cv.ReleaseImage(frame1);
+                    Cv.ReleaseImage(frame2);
+                    Cv.ReleaseImage(frame3);
+                    Cv.ReleaseImage(frameResized1);
+                    Cv.ReleaseImage(frameResized2);
+                    Cv.ReleaseImage(frameResized3);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("何らかのエラーです。\n\nカメラが正しく接続され、\nIndex番号が正しく入力されているか確認して下さい。",
+                "エラー",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            finishFlag = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CvCapture test = Cv.CreateCameraCapture(3);
+            if (test == null)
+            {
+                label5.Text = "取得失敗";
+            }
+            else
+            {
+                IplImage test1 = Cv.QueryFrame(test);
+
+            }
+        }
+    }
+}
